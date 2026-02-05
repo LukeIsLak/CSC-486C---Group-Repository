d27bd2e6ac3e78830df1e9d8176d0d4ea17eba9f
ArchieSherwood <archiesherwoods@gmail.com>
2026-02-04 20:59:36 -0800

    Added Attack and Modifier cards
    Created two objects under the card class for attack and modifier cards, further work includes handling the attack and stat changes

--- Assets/Scripts/Cards.cs
+++ Assets/Scripts/Cards.cs
@@ -2,16 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu]
-//Create a class to hold all the information for the cards
+//Create a class to hold the information for the cards
 public class Cards : ScriptableObject
 {
     public string name = null;
     public Sprite image = null;
 
-    public string element = null;
+}
 
-    public string attackType = null;
+[CreateAssetMenu(menuName = "Cards/AttackCards")]
+public class AttackCards : Cards
+{
+    public int attackDamage;
+    public string element;
+    public string attackType;
 
-    //public int attackDamage = null;
+}
+
+[CreateAssetMenu(menuName = "Cards/ModifierCards")]
+public class ModifierCards : Cards
+{
+    public string effect;
+    public int value;
 }
