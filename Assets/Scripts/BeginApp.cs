561ee5228d19fafcf1ec2ca115194f2c4efdefbd
Compiavelli <nate.manke@gmail.com>
2026-02-17 14:28:39 -0800

    Fog fading logic

--- Assets/Scripts/BeginApp.cs
+++ Assets/Scripts/BeginApp.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TestApp : MonoBehaviour
@@ -9,14 +10,13 @@ public class TestApp : MonoBehaviour
 
     void Start()
     {
-            // Transition from lobby to layout
-            layoutData.depth            = 9;
-            layoutData.maxWidth         = 7;
-            layoutData.randomSeed       = 0;
-            layoutData.useSeed          = true;
-            layoutData.shouldGenerate   = true;
-            layoutData.completedIndices.Clear();
-            layoutData.completedIndices.Add(0);
+        // Transition from lobby to layout
+        layoutData.depth            = 9;
+        layoutData.maxWidth         = 7;
+        layoutData.randomSeed       = 0;
+        layoutData.useSeed          = true;
+
+        layoutData.InitializeStates();
     }
     void Update()
     {
