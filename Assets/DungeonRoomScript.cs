c3006effb08aa9b66cca707a338d64d5e46f3b99
Compiavelli <nate.manke@gmail.com>
2026-02-26 22:38:31 -0800

    Framework for trap/regular room spawning and initialization

---
+++ Assets/DungeonRoomScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonRoomScript : MonoBehaviour
+{
+    public bool isTrapRoom;
+
+    // If we contain everything needed to make each room type function in a prefab, 
+    // We can just instantiate things as needed.
+    public GameObject trapRoomObjects; 
+    public GameObject regularRoomObjects;
+    public void Initialize()
+    {
+        if (isTrapRoom) 
+        {
+            TrapRoomInit();
+            return;
+        }
+        RegularRoomInit();
+    }
+
+    public void SetTrapRoom()
+    {
+        isTrapRoom = true;
+    }
+
+    private void TrapRoomInit()
+    {
+        // Create necessary objects for trap room functionality
+        return;
+    }
+
+    private void RegularRoomInit()
+    {
+        // Create necessary objects for regular room functionality
+        return;
+    }
+
+
+}
