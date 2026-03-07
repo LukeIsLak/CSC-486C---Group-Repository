1bbf61d2cc83138556f1ed122532c23579a917b4
Luke <62114548+LukeIsLak@users.noreply.github.com>
2026-03-06 19:09:20 -0800

    Modified state machines and rat colonies to properly add and remove entities as well as handle removal of lonely rats

--- Assets/Scripts/Enemy/Rat/Rat.cs
+++ Assets/Scripts/Enemy/Rat/Rat.cs
@@ -61,7 +61,7 @@ public class Rat : EnemyInterface
         
         rcm = env_rcm;
         rsm = env_rsm;
-        rsm.entities.Add(this);
+        rsm.AddEntity(this);
 
         outerCol.radius = rd.colonyDist;
         innerCol.radius = rd.neighbourStopRadius;
@@ -79,6 +79,7 @@ public class Rat : EnemyInterface
     public override void KillEnemy() {
         if (trs != null) trs.RemoveEnemy();
         rcm.RemoveRat(this, ratColonyNum);
+        rsm.RemoveEntity(this);
         Destroy(this.gameObject);
     }
 
