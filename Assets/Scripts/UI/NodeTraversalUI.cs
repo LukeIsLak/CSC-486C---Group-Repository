6c4beec4f94102cd5b45f32242a3e6dcc52e410b
Khanh150104 <khanh150104@gmail.com>
2026-03-08 16:38:06 -0700

    add build buffer deck and fix scrolling layout of the player inventory

--- Assets/Scripts/UI/NodeTraversalUI.cs
+++ Assets/Scripts/UI/NodeTraversalUI.cs
@@ -5,21 +5,27 @@ using UnityEngine.UI;
 
 public class NodeTraversalUI : MonoBehaviour
 {
-    [SerializeField] private RectTransform containerLocation;
+    [SerializeField] private RectTransform playerDeckLocation;
+    [SerializeField] private RectTransform bufferLocation;
     [SerializeField] private GameObject inventoryInNode;
     [SerializeField] private PlayerInventory playerInventory;
     [SerializeField] private CardViewUI cardViewPrefab;
-    [SerializeField] private ScrollRect scrollRect; // For forcing showing item from the top
+    [SerializeField] private ScrollRect playerDeckScrollRect; // For forcing showing item from the top
+    [SerializeField] private ScrollRect bufferDeckScrollRect; // For forcing showing item from the top
     [SerializeField] private GameObject openDeckButton;
-    private List<CardViewUI> cards = new();
+    private List<CardViewUI> playerDeckCards = new();
+    private List<CardViewUI> bufferCards = new();
+    
 
     public void ShowDeckContainer()
     {
         openDeckButton.SetActive(false);
         inventoryInNode.SetActive(true);
         BuildDeckUI();
+        BuildBufferUI();
         Canvas.ForceUpdateCanvases();
-        scrollRect.verticalNormalizedPosition = 1f; // top
+        playerDeckScrollRect.verticalNormalizedPosition = 1f; // top
+        bufferDeckScrollRect.verticalNormalizedPosition = 1f;
     }
 
     public void HideDeckContainer()
@@ -35,20 +41,50 @@ public class NodeTraversalUI : MonoBehaviour
         foreach (var instance in playerInventory.playerdeck)
         {
 
-            CardViewUI card = Instantiate(cardViewPrefab, containerLocation);
+            CardViewUI card = Instantiate(cardViewPrefab, playerDeckLocation);
             
             card.Init(instance.cardData, !instance.useable);
-            cards.Add(card);
+            playerDeckCards.Add(card);
         }
 
+    }
+
+    private void BuildBufferUI()
+    {
+        foreach (var instance in playerInventory.buffer)
+        {
+            CardViewUI card = Instantiate(cardViewPrefab, bufferLocation);
+
+            card.Init(instance.cardData, !instance.useable);
+            bufferCards.Add(card);
+        }
+    }
+
+    private void OnBufferCardClicked(CardInstance card)
+    {
+        playerInventory.bufferToDeck(card);
+
+    }
+
+    private void RefreshUI()
+    {
+        ClearDeckUI();
+        BuildDeckUI();
+        BuildBufferUI();
     }
     private void ClearDeckUI()
     {
         // Destroy all existing UI card gameObjects.
-        foreach (var card in cards)
+        foreach (var card in playerDeckCards)
         {
             Destroy(card.gameObject);
         }
-        cards.Clear();
+        playerDeckCards.Clear();
+
+        foreach (var card in bufferCards)
+        {
+            Destroy(card.gameObject);
+        }
+        bufferCards.Clear();
     }
 }
