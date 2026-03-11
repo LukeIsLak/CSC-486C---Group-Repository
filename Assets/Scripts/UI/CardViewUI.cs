434a75b5d3aa3db62e3ffba5f4d1d9495cc49b94
Compiavelli <nate.manke@gmail.com>
2026-03-11 14:46:49 -0700

    Fix reward spawning; update cardviewui to display info

--- Assets/Scripts/UI/CardViewUI.cs
+++ Assets/Scripts/UI/CardViewUI.cs
@@ -10,14 +10,17 @@ public class CardViewUI : MonoBehaviour
     public TextMeshProUGUI cardName;
     public Image cardImage;
     public TextMeshProUGUI cardDamage;
+    public TextMeshProUGUI cardDescription;
     public Button button;
+
     public void Init(Cards cardData, bool isGrayOut = false, System.Action onCardClick = null, bool buttonDisable = false)
     {
         card = cardData;    
         cardImage.sprite = card.image;
         if(isGrayOut) cardImage.color = new Color32(176, 176, 176, 255);
         cardName.text = cardData.name;
-        //cardDamage.text = cardData.
+        cardDamage.text = cardData.effectValue.ToString();
+        cardDescription.text = cardData.descrption;
 
         if(button != null)
         {
