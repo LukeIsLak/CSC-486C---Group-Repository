11afc68d5dd7aa711384b1de569f49e7009edaa4
ArchieSherwood <archiesherwoods@gmail.com>
2026-03-26 02:09:08 -0700

    Updated Astral Dagger
    Updated Astral Dagger code, gave it a ttl and shooting from the camera not the player. And attached the new sprite

--- Assets/Cards/Card Scriptable Object Scripts/AstralDagger.cs
+++ Assets/Cards/Card Scriptable Object Scripts/AstralDagger.cs
@@ -9,15 +9,19 @@ public class AstralDagger : Cards
     [SerializeField] private DaggerObject daggerPrefab;
 
     private GameObject player;
+    private GameObject camera;
+
 
     
 
     public override IEnumerator Play(Cards card){
         player = GameObject.FindWithTag("Player");
+        camera = GameObject.FindWithTag("MainCamera");
+
 
         //Instantiate the object and start the homing routine
 
-        DaggerObject dagger = Instantiate(daggerPrefab, player.transform.position + player.transform.forward * 2f, player.transform.rotation);
+        DaggerObject dagger = Instantiate(daggerPrefab, camera.transform.position + camera.transform.forward * 2f, camera.transform.rotation);
 
         HomingSystem homing = dagger.GetComponent<HomingSystem>();
         homing.Initialize();
