e6d4e5156e51383deabc243a8bb6649ba58960b8
irizzzz <44013230+irizzzz@users.noreply.github.com>
2026-03-26 02:06:50 -0700

    figured out a better way to do enemy hit fx lmao

--- Assets/Scripts/Player/PlayerCharacter.cs
+++ Assets/Scripts/Player/PlayerCharacter.cs
@@ -130,7 +130,7 @@ public class PlayerCharacter : MonoBehaviour
             //Debug.Log($"Hit: {hit.collider.name} ");
 
             var enemyComponent = hit.collider.GetComponentInParent<EnemyInterface>();
-            if (enemyComponent != null) enemyComponent.Hit(attackMultiplier * attackDamage);
+            if (enemyComponent != null) enemyComponent.GetComponent<FMODUnity.StudioEventEmitter>().Play(); enemyComponent.Hit(attackMultiplier * attackDamage);
             TriggerHitStop(attackHitStopDuration);
             
         }
