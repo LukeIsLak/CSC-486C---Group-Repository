8109aef0599aba4e39b526e36f5b1f42135767c4
Compiavelli <nate.manke@gmail.com>
2026-03-29 20:27:50 -0700

    Fix gold drop error; rebind player controls

--- Assets/Scripts/GoldDropSpawner.cs
+++ Assets/Scripts/GoldDropSpawner.cs
@@ -6,10 +6,18 @@ public class GoldDropSpawner : MonoBehaviour
 {
     public GameObject goldDropPrefab;
     public int amount = 1;
+
+    private bool isQuitting = false;
     void OnDestroy()
     {
+        if (!Application.isPlaying || isQuitting) return;
         GameObject goldDrop = Instantiate(goldDropPrefab);
         goldDrop.transform.position = transform.position;
         goldDrop.GetComponent<GoldDrop>().DoGoldAcquire(amount);
+    }
+
+    void OnApplicationQuit()
+    {
+        isQuitting = true;
     }
 }
