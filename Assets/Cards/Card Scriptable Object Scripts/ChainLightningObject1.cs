c411b4732f170ef51bf85b88c58cc6bc0fad2743
Compiavelli <126530451+NathanManke@users.noreply.github.com>
2026-04-16 12:06:07 -0700

    Update chain lightning

---
+++ Assets/Cards/Card Scriptable Object Scripts/ChainLightningObject1.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChainLightningObject1 : MonoBehaviour
+{
+
+    [SerializeField] private float damage = 10f;
+    [SerializeField] private float radius = 10f;
+    [SerializeField] private int chainNumber = 0;
+    [SerializeField] private int maxChain = 5;
+    [SerializeField] private Vector3 targetPos;
+
+    public List<EnemyInterface> alreadyHit = new List<EnemyInterface>();
+
+    [SerializeField] private GameObject ChainLightningPrefab;
+    [SerializeField] private GameObject lightningPrefab;
+    
+
+
+    public void Init(float dmg, float maxRange, int chainNum, int chainMax, List<EnemyInterface> pastHits)
+    {
+        // Set values
+        damage = dmg;
+        radius = maxRange;
+        chainNumber = chainNum;
+        maxChain = chainMax;
+        alreadyHit = pastHits;
+        // Do lightning logic
+        Fire();
+        StartCoroutine(HandleNext());
+    }
+
+    private void Fire()
+    {
+        List<EnemyInterface> nearbyEnemies = new();
+
+        // Check for enemies in radius
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius);
+        foreach (var hitCollider in hitColliders)
+        {
+            EnemyInterface ei = hitCollider.GetComponentInParent<EnemyInterface>();
+            if (!ei) continue;
+
+            if (alreadyHit.Contains(ei)) continue;
+            // Check if visible 
+            Vector3 direction = hitCollider.transform.position - transform.position;
+
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, direction.normalized, out hit, direction.magnitude, LayerMask.GetMask("Surface"))) 
+            {
+                continue;
+            }
+
+            nearbyEnemies.Add(ei);
+        }
+        if (nearbyEnemies.Count == 0) return;
+
+        // Order list by proximity, get earliest
+        EnemyInterface target = nearbyEnemies.OrderBy(c => (transform.position - c.transform.position).sqrMagnitude).ToArray()[0];
+        target.Hit(damage);
+        alreadyHit.Add(target);
+        targetPos = target.transform.position;
+
+        // Draw graphics
+        LightningBoltObject lightning = Instantiate(lightningPrefab).GetComponent<LightningBoltObject>();
+        LineRenderer line = lightning.GetComponent<LineRenderer>();
+        int points = 6;
+        line.positionCount = points;
+        Vector3 start = transform.position;
+        for (int i = 0; i < points; i++)
+        {
+            float t = (float)i / (points - 1);
+            Vector3 pos = Vector3.Lerp(start, targetPos, t);
+
+            pos += new Vector3(
+                Random.Range(-0.1f, 0.1f),
+                Random.Range(-0.1f, 0.1f),
+                Random.Range(-0.1f, 0.1f)
+            );
+            line.SetPosition(i, pos);
+        }
+    }
+
+    private IEnumerator HandleNext()
+    {
+        if (chainNumber >= maxChain) yield break;
+        yield return new WaitForSeconds(0.1f);
+        GameObject nextLightning = Instantiate(ChainLightningPrefab);
+        nextLightning.transform.position = targetPos;
+        nextLightning.GetComponent<ChainLightningObject1>().Init(damage, radius, chainNumber + 1, maxChain, alreadyHit);
+        Destroy(this);
+    }
+}
